9ffacbaa3535f01846830b6b84ef8125ab31abaa
Vid Kotnik <vid.kotnik@stroka.si>
2024-03-06 15:20:54 +0100

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const morgan = require("morgan");
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
+
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
     console.log("API requested");
